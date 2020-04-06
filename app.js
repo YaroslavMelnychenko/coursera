902b51588c64c33a0696b4ad1833769feaa62ac9
Yaroslav Melnychenko <jaroslav.melnychenko@gmail.com>
2020-04-06 22:27:21 +0300

    added basic authentication

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ var leadersRouter = require('./routes/leaders');
 const mongoose = require('mongoose');
 
 const url = 'mongodb://localhost:27017/application';
-const connect = mongoose.connect(url);
+const connect = mongoose.connect(url, { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
+});
 
 connect.then((db) => {
   console.log('Connection to mongodb is successfull');
@@ -30,6 +33,35 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+
+  if(!authHeader) {
+    var err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    next(err);
+    return;
+  }
+
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+
+  var username = auth[0];
+  var password = auth[1];
+
+  if(username == 'admin' && password == 'password') next();
+  else {
+    var err = new Error('Authentication failed');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    next(err);
+  }
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);

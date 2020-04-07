b2f0c65285c235fe8bb4da81cb5c63d769b71938
Yaroslav Melnychenko <jaroslav.melnychenko@gmail.com>
2020-04-07 22:28:09 +0300

    added passport token-based authentication with jwt

--- routes/leaders.js
+++ routes/leaders.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const authenticate = require('../authenticate');
 
 const Leaders = require('../models/leaders');
 
@@ -18,12 +19,12 @@ leadersRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 405;
     res.setHeader('Content-Type', 'text/plain');
     res.end('PUT method is not allowed');
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser, (req, res, next) => {
     Leaders.create(req.body)
     .then((leader) => {
         console.log('Leader created\n', leader);
@@ -33,7 +34,7 @@ leadersRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
     Leaders.remove({})
     .then((result) => {
         res.statusCode = 200;
@@ -53,7 +54,7 @@ leadersRouter.route('/:leaderId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
     Leaders.findByIdAndUpdate(req.params.leaderId, {
         $set: req.body
     }, {
@@ -66,12 +67,12 @@ leadersRouter.route('/:leaderId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 405;
     res.setHeader('Content-Type', 'text/plain');
     res.end('POST method is not allowed');
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
     Leaders.findByIdAndRemove(req.params.leaderId)
     .then((result) => {
         res.statusCode = 200;

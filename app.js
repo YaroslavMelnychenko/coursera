492dbfff97cfd6184a0bb5063a8ee683aa535856
Yaroslav Melnychenko <jaroslav.melnychenko@gmail.com>
2020-04-07 20:34:29 +0300

    added passport session + mongodb auth

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ var usersRouter = require('./routes/users');
 var promotionsRouter = require('./routes/promotions');
 var leadersRouter = require('./routes/leaders');
 
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
 const mongoose = require('mongoose');
 
 const url = 'mongodb://localhost:27017/application';
@@ -45,41 +48,22 @@ app.use(session({
   store: new FileStore()
 }));
 
-function auth (req, res, next) {
-  console.log(req.session);
+app.use(passport.initialize());
+app.use(passport.session());
 
-  if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');                        
-      err.status = 401;
-      next(err);
-      return;
-    }
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == 'admin' && pass == 'password') {
-      req.session.user = 'admin';
-      next(); // authorized
-    } else {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-    }
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+function auth (req, res, next) {
+  console.log(req.user);
+
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
   }
   else {
-    if (req.session.user === 'admin') {
-      console.log('req.session: ',req.session);
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 401;
-      next(err);
-    }
+    next();
   }
 }
 
@@ -87,8 +71,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/promotions', promotionsRouter);
 app.use('/leaders', leadersRouter);
 

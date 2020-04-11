baff4618e349cf7000a4081f08eac74f65046be5
Yaroslav Melnychenko <jaroslav.melnychenko@gmail.com>
2020-04-11 21:17:38 +0300

    added facebook OAuth

--- routes/users.js
+++ routes/users.js
@@ -53,4 +53,13 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
   res.json({success: true, token, status: 'You are successfully logged in!'});
 });
 
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res, next) => {
+  if(req.user) {
+    var token = authenticate.getToken({ _id: req.user._id });
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({ success: true, token, status: 'You are successfully logged in using facebook!' });
+  }
+});
+
 module.exports = router;
